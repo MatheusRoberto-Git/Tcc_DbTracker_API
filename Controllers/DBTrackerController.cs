259d288d6e689bfe276d2186444aefee7437f4bb
Mathe <Mathe@DESKTOP-FNHK3S8>
2024-09-28 09:30:17 -0300

    Add project files.

---
+++ Controllers/DBTrackerController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tcc_DbTracker_API.Repository;
+
+namespace Tcc_DbTracker_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DBTrackerController : ControllerBase
+    {
+        #region [Construtor]
+
+        private readonly Conn_SqlServer _sqlServer;
+
+        public DBTrackerController(Conn_SqlServer sqlServer)
+        {
+            _sqlServer = sqlServer;
+        }
+
+        #endregion
+
+        #region [Get Tables]
+
+        [HttpGet("getAllTables")]
+        public IActionResult GetAllTables()
+        {
+            IEnumerable<string> allTables = _sqlServer.ListarTabelas();
+            return Ok(allTables);
+        }
+
+        [HttpGet("getAllTables/structures")]
+        public ActionResult<IEnumerable<object>> GetAllTableStructures()
+        {
+            var tabelas = _sqlServer.ListarTabelas();
+
+            var estruturas = tabelas.Select(tabela => new
+            {
+                TableName = tabela,
+                Structure = _sqlServer.EstruturaTabelas(tabela)
+            }).ToList();
+
+            return Ok(estruturas);
+        }
+
+        [HttpGet("{nomeTabela}/structure")]
+        public ActionResult<IEnumerable<dynamic>> GetTableStructure(string nomeTabela)
+        {
+            if (string.IsNullOrEmpty(nomeTabela))
+            {
+                return BadRequest("O nome da tabela não pode ser nulo ou vazio.");
+            }
+
+            IEnumerable<dynamic> estrutura = _sqlServer.EstruturaTabelas(nomeTabela);
+
+            if (estrutura == null || !estrutura.Any())
+            {
+                return NotFound($"A tabela '{nomeTabela}' não foi encontrada.");
+            }
+
+            var result = new
+            {
+                TableName = nomeTabela,
+                Structure = estrutura
+            };
+
+            return Ok(result);
+        }
+
+        #endregion
+    }
+}
---
+++ Repository/Conn_SqlServer.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+
+namespace Tcc_DbTracker_API.Repository
+{
+    public class Conn_SqlServer
+    {
+        #region [Construtor]
+
+        private readonly string _connectionString;
+
+        public Conn_SqlServer(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        #endregion
+
+        #region [Tabelas]
+
+        public IEnumerable<string> ListarTabelas()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE';";
+
+                return connection.Query<string>(query);
+            }
+        }
+
+        public IEnumerable<dynamic> EstruturaTabelas(string nmTabela)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string query = @"
+                SELECT 
+                    COLUMN_NAME, 
+                    DATA_TYPE, 
+                    IS_NULLABLE, 
+                    CHARACTER_MAXIMUM_LENGTH 
+                FROM INFORMATION_SCHEMA.COLUMNS 
+                WHERE TABLE_NAME = @TabelaNome";
+
+                return connection.Query(query, new { TabelaNome = nmTabela });
+            }
+        }
+
+        #endregion
+    }
+}
